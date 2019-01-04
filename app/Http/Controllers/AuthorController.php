a9ab2fa353afe9d991d753b023f6bbe5465d94a4
Lucas Cardial <luccascardial@gmail.com>
2019-01-04 11:42:10 -0300

    Projects

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controller;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($author)
+    {
+
+    }
+
+    public function update(Request $request, $author)
+    {
+        # code...
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Models\Author;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Author::class, 50)->create();
+    }
+}
